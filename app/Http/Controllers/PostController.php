fa36494a575227c69ed8a5b53267accdc744dda9
Eknoorpreet Singh <eknoorpreet2@gmail.com>
2024-06-22 10:53:22 -0700

    implemented MustBeLoggedIn middleware

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Models\Post;
+use Illuminate\Support\Str;
 use Illuminate\Http\Request;
 
 class PostController extends Controller
@@ -33,6 +34,7 @@ class PostController extends Controller
     // Laravel will automatically query the database through the lens of the Post model
     public function viewPost(Post $post)
     {
+        $post['body'] = Str::markdown($post->body);
         return view('single-post', ['post' => $post]);
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -16,14 +16,14 @@ use App\Http\Controllers\UserController;
 |
 */
 
-Route::get('/', [UserController::class, "showCorrectHomepage"]);
+Route::get('/', [UserController::class, "showCorrectHomepage"])->name('login');
 
 // Auth routes
-Route::post('/register', [UserController::class, "register"]);
-Route::post('/login', [UserController::class, "login"]);
-Route::post('/logout', [UserController::class, "logout"]);
+Route::post('/register', [UserController::class, "register"])->middleware('guest');
+Route::post('/login', [UserController::class, "login"])->middleware('guest');
+Route::post('/logout', [UserController::class, "logout"])->middleware('mustBeLoggedIn');
 
 // Blog-related routes
-Route::get('/create-post', [PostController::class, 'showCreateForm']);
-Route::post('/create-post', [PostController::class, 'createPost']);
+Route::get('/create-post', [PostController::class, 'showCreateForm'])->middleware('mustBeLoggedIn');
+Route::post('/create-post', [PostController::class, 'createPost'])->middleware('mustBeLoggedIn');
 Route::get('/post/{post}', [PostController::class, 'viewPost']);
